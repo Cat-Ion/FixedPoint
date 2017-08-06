2a374f8f91d95cd022f5904e4de16cc70862b59f
Valentin Ochs <a@0au.de>
2017-08-06 19:44:29 +0200

    Move MWI cast operators to separate header

--- FixedPoint.hpp
+++ FixedPoint.hpp
@@ -325,70 +325,12 @@ public:
         }
     }
 
-    constexpr
-    operator bool() const {
-        for (unsigned i = 0; i < size; i++) {
-            if (s[i]) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    explicit constexpr
-    operator int8_t() const {
-        return s[0] & 0xFF;
-    }
-
-    explicit constexpr
-    operator int16_t() const {
-        int16_t r = 0;
-        for (unsigned i = 0, j = 0; i < 16; i += storageSize) {
-            r |= s[j++] << i;
-        }
-        return r;
-    }
-
-    explicit constexpr
-    operator int32_t() const {
-        int32_t r = 0;
-        for (unsigned i = 0, j = 0; i < 32; i += storageSize) {
-            r |= s[j++] << i;
-        }
-        return r;
-    }
-
-    explicit constexpr
-    operator int64_t() const {
-        int64_t r = 0;
-        for (unsigned i = 0, j = 0; i < 64; i += storageSize) {
-            r |= s[j++] << i;
-        }
-        return r;
-    }
-
-    explicit constexpr
-    operator double() const {
-        double r = 0;
-        double m = pow(2., storageSize);
-        double n = 1;
-
-        if (this->is_positive()) {
-            return -double(-*this);
-        }
-
-        bigType c = 1;
-
-        for (unsigned i = 0; i < size; i++) {
-            c += static_cast<storageType>(~s[i]);
-            storageType u = c;
-            r += n * u;
-            n *= m;
-            c >>= storageSize;
-        }
-
-        return -r;
-    }
+    explicit constexpr operator bool() const;
+    explicit constexpr operator int8_t() const;
+    explicit constexpr operator int16_t() const;
+    explicit constexpr operator int32_t() const;
+    explicit constexpr operator int64_t() const;
+    explicit constexpr operator double() const;
 
     constexpr
     unsigned
@@ -845,4 +787,5 @@ namespace std {
 #include "MultiwordIntegerConstructors.hpp"
 #include "MultiwordIntegerArithmetics.hpp"
 #include "MultiwordIntegerBitwise.hpp"
+#include "MultiwordIntegerCasts.hpp"
 #endif // FIXEDPOINT_HPP
