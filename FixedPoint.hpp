e51f1602fc2973d1155bfbf841c05aacb09e7172
Valentin Ochs <a@0au.de>
2017-08-06 19:32:17 +0200

    Move MultiwordInteger bitwise functions to separate header

--- FixedPoint.hpp
+++ FixedPoint.hpp
@@ -99,120 +99,12 @@ public:
     constexpr MultiwordInteger<size, storageType>& operator--();
     constexpr MultiwordInteger<size, storageType>& operator--(int);
 
-    constexpr MultiwordInteger<size, storageType>&
-    operator &=(MultiwordInteger<size, storageType> const &o) {
-        for (unsigned i = 0; i < size; i++) {
-            s[i] &= o.s[i];
-        }
-        return *this;
-    }
-
-    constexpr MultiwordInteger<size, storageType>&
-    operator |=(MultiwordInteger<size, storageType> const &o) {
-        for (unsigned i = 0; i < size; i++) {
-            s[i] |= o.s[i];
-        }
-        return *this;
-    }
-
-    constexpr MultiwordInteger<size, storageType>&
-    operator ^=(MultiwordInteger<size, storageType> const &o) {
-        for (unsigned i = 0; i < size; i++) {
-            s[i] ^= o.s[i];
-        }
-        return *this;
-    }
-
-    constexpr MultiwordInteger<size, storageType>
-    operator~() const {
-        MultiwordInteger<size, storageType> r;
-        for (unsigned i = 0; i < size; i++) {
-            r.s[i] = ~s[i];
-        }
-        return r;
-    }
-
-    constexpr
-    MultiwordInteger<size, storageType>&
-    operator<<=(unsigned n) {
-        size_t w = n / storageSize;
-        n %= storageSize;
-
-        if (w >= size) {
-            *this = storageType(0);
-            return *this;
-        }
-
-        if (n == 0) {
-            for (unsigned i = size; i-- > w; ) {
-                s[i] = s[i-w];
-            }
-            for (unsigned i = 0; i < w; i++) {
-                s[i] = 0;
-            }
-            return *this;
-        }
-
-        unsigned i = size;
-        while (i-- > (1+w)) {
-            this->s[i] = (this->s[i - w] << n)
-                    + (this->s[i - w - 1] >> (storageSize - n));
-        }
-        this->s[i] = (this->s[i - w] << n) ;
-        while (i-- > 0) {
-            this->s[i] = 0;
-        }
-
-        return *this;
-    }
-
-    constexpr
-    MultiwordInteger<size, storageType>&
-    operator>>=(unsigned n) {
-        size_t w = n / storageSize;
-        n %= storageSize;
-
-        bool adjust_leading_bits = this->is_negative();
-
-        if (w >= size) {
-            if (adjust_leading_bits) {
-                for (unsigned i = 0; i < size; i++) {
-                    s[i] = ~static_cast<storageType>(0);
-                }
-            } else {
-                *this = storageType(0);
-            }
-            return *this;
-        }
-
-        if (n == 0) {
-            for (unsigned i = 0; i < size-w; i++) {
-                s[i] = s[i+w];
-            }
-            for (unsigned i = size-w; i < size; i++) {
-                s[i] = 0;
-            }
-            if (adjust_leading_bits) {
-                this->fill_leading_bits(w*storageSize);
-            }
-            return *this;
-        }
-
-        unsigned i = 0;
-        while (i < (size - w - 1)) {
-            this->s[i] = (this->s[i + w] >> n) + (this->s[i + w + 1] << (storageSize - n));
-            i++;
-        }
-        this->s[i] = this->s[i + w] >> n;
-        while (++i < size) {
-            this->s[i] = 0;
-        }
-
-        if (adjust_leading_bits) {
-            this->fill_leading_bits(w * storageSize + n);
-        }
-        return *this;
-    }
+    constexpr MultiwordInteger<size, storageType>& operator &=(MultiwordInteger<size, storageType> const &o);
+    constexpr MultiwordInteger<size, storageType>& operator |=(MultiwordInteger<size, storageType> const &o);
+    constexpr MultiwordInteger<size, storageType>& operator ^=(MultiwordInteger<size, storageType> const &o);
+    constexpr MultiwordInteger<size, storageType>  operator ~() const;
+    constexpr MultiwordInteger<size, storageType>& operator <<=(unsigned n);
+    constexpr MultiwordInteger<size, storageType>& operator >>=(unsigned n);
 
     constexpr
     MultiwordInteger<size, storageType>
@@ -1004,4 +896,5 @@ namespace std {
 
 #include "MultiwordIntegerConstructors.hpp"
 #include "MultiwordIntegerArithmetics.hpp"
+#include "MultiwordIntegerBitwise.hpp"
 #endif // FIXEDPOINT_HPP
